29accd9d6030b51aa1711f2047e8ee6293c2c14c
Prajwal <prjwl888.dkc@gmail.com>
2022-04-08 23:39:30 +0530

    modified login.js and register.js files

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 export default function Login() {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    reEnterPassword: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
   return (
     <div className="login">
       <h1>LOGIN</h1>
-      <input type="text" placeholder="Enter your email" />
-      <input type="password" placeholder="Enter password" />
+      <input
+        type="text"
+        name="email"
+        value={user.email}
+        placeholder="Enter your email"
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={user.password}
+        placeholder="Enter password"
+        onChange={handleChange}
+      />
       <div className="button">Login</div>
       <div>or</div>
       <div className="button">Register</div>
--- client/src/components/register/register.js
+++ client/src/components/register/register.js
@@ -7,14 +7,46 @@ export default function Register() {
     password: "",
     reEnterPassword: "",
   });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
 
   return (
     <div className="register">
+      {console.log(user)}
       <h1>REGISTER</h1>
-      <input type="text" placeholder="your name" />
-      <input type="text" placeholder="your email" />
-      <input type="password" placeholder="your Password" />
-      <input type="password" placeholder="Confirm Password" />
+      <input
+        type="text"
+        name="name"
+        value={user.name}
+        placeholder="your name"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="email"
+        value={user.email}
+        placeholder="your email"
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={user.password}
+        placeholder="your Password"
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="reEnterPassword"
+        value={user.reEnterPassword}
+        placeholder="Confirm Password"
+        onChange={handleChange}
+      />
       <div className="button">REGISTER</div>
       <div>or</div>
       <div className="button">LOGIN</div>
