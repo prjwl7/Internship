f1901574ffc194fdaf2eebaf6230e9cb527e2e18
Prajwal <prjwl888.dkc@gmail.com>
2022-04-08 22:54:54 +0530

    styled app.css and starting to work on homepage

--- client/src/components/homepage/homepage.js
+++ client/src/components/homepage/homepage.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "./homepage.css";
 export default function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <div className="homepage">
+      <h1>Hello Homepage</h1>
+      <div className="button">Logout</div>
+    </div>
+  );
 }
